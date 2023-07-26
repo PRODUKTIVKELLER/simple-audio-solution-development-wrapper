ad62ffd3f678e855e2a15798873f5afb9852a3f7
Stefan <stefan.produktivkeller@gmail.com>
2023-07-27 00:59:33 +0200

    [FEATURE] Update package.

---
+++ Assets/Scripts/CrossFadeSoundEmitterApiForButtons.cs
@@ -0,0 +1,17 @@
+﻿using Produktivkeller.SimpleAudioSolution.Emitter;
+using UnityEngine;
+
+public class CrossFadeSoundEmitterApiForButtons: MonoBehaviour
+{
+    [SerializeField] private CrossFadeSoundEmitter crossFadeSoundEmitter;
+    
+    public void ToFirst()
+    {
+        crossFadeSoundEmitter.CrossFadeTo(0);
+    }
+
+    public void ToSecond()
+    {
+        crossFadeSoundEmitter.CrossFadeTo(1);
+    }
+}
